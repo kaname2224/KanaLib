bcc4c1d20e10e331acc687ccbea8478863fe3d9d
Kaname <support@webcord.fr>
2022-08-26 08:12:57 +0200

    Initial commit

---
+++ src/main/java/fr/kaname/kanalib/KanaLib.java
@@ -0,0 +1,11 @@
+package fr.kaname.kanalib;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class KanaLib extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        this.getLogger().info("Stating KanaLib");
+    }
+}
